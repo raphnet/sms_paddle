382b019fe17ecc1ab4af55ef6d99fafb6cec4bdc
Raphael Assenat <raph@raphnet.net>
2015-03-01 15:05:19 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,54 @@
+CC=avr-gcc
+AS=$(CC)
+LD=$(CC)
+
+CPU=atmega8
+AVRDUDE=avrdude -p m8 -P usb -c avrispmkII
+CFLAGS=-Wall -mmcu=$(CPU) -Os -DF_CPU=8000000L
+LDFLAGS=-mmcu=$(CPU) -Wl,-Map=mapfile.map
+
+OBJS=smspaddle.o
+PROG=smspaddle
+
+# RSTDISBL  WDTON  SPIEN  CKOPT  EESAVE  BOOTSZ1  BOOTSZ0  BOOTRST
+#    1        1      0      1      1        0        0        1
+HFUSE=0xd9
+
+# BODLEVEL  BODEN  SUT1  SUT0  CKSEL3  CKSEL2  CKSEL1  CKSEL0
+#    1        1      1    0      0       1       0       0
+#
+# Note: Uses internal 8MHz RC Oscillator
+LFUSE=0xE4
+
+all: $(PROG).hex
+
+clean:
+	rm -f $(PROG).elf $(PROG).hex $(PROG).map $(OBJS)
+
+$(PROG).elf: $(OBJS)
+	$(LD) $(OBJS) $(LDFLAGS) -o $(PROG).elf
+
+$(PROG).hex: $(PROG).elf
+	avr-objcopy -j .data -j .text -O ihex $(PROG).elf $(PROG).hex
+	avr-size $(PROG).elf
+
+flash: $(PROG).hex
+	$(AVRDUDE) -Uflash:w:$< -B 5.0 -e
+
+fuse:
+	$(AVRDUDE) -e -Uhfuse:w:$(HFUSE):m -Ulfuse:w:$(LFUSE):m -B 20.0 -F
+
+erase:
+	$(AVRDUDE) -B 10.0 -e
+
+reset:
+	$(AVRDUDE) -B 10.0
+
+%.o: %.S
+	$(CC) $(CFLAGS) -c $<
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $<
+
+%.o: %.c %.h
+	$(CC) $(CFLAGS) -c $<
---
+++ smspaddle.c
@@ -0,0 +1,154 @@
+/* DIY SMS/MARKIII Paddle
+ * Copyright (C) 2015 Raphaël Assénat
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * The author may be contacted at raph@raphnet.net
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define EXPORT_PADDLE_SUPPORT	1
+
+#define STATE_HPD_INIT			1
+#define STATE_HPD_RUNNING		2
+#define STATE_EXPORT_INIT		3
+#define STATE_EXPORT_RUNNING	4
+
+int main(void)
+{
+	unsigned char value = 0x00;
+	unsigned char state = STATE_HPD_INIT;
+
+	/* PORTB
+	 * 0: Output low (mode switch)
+	 * 1: Input with pull up (mode switch)
+	 * 2-7: Input with pull up (unused)
+	 */
+	DDRB = 0x01;
+	PORTB = 0xFE;
+
+	/*
+	 * PORTD
+	 *
+	 * 2: Input for export paddle
+	 */
+	DDRD = 0x00;
+	PORTD = 0xFF;
+
+
+	/* PORTC
+	 *
+	 * 0: ADC0 input
+	 * 1: TR output
+	 * 2: D0/D4 output
+	 * 3: D1/D5 output
+	 * 4: D2/D6 output
+	 * 5: D3/D7 output
+	 * 6: /RESET
+	 */
+	DDRC = 0xFE;
+	PORTC = 0x00;
+
+#ifdef EXPORT_PADDLE_SUPPORT
+	_delay_ms(10);
+	// PB1 is low when the switch is closed. This enables "Export paddle" mode
+	if (!(PINB & 0x02)) {
+		state = STATE_EXPORT_INIT;
+	}
+#endif
+
+	ADMUX = (1<<ADLAR) | (1<<REFS0); // AVCC reference, channel 0 (ADC0), left adjusted result
+
+	while(1)
+	{
+		switch (state)
+		{
+			default:
+				state = STATE_HPD_INIT;
+				break;
+
+			case STATE_HPD_INIT:
+				// prescaler set to /32
+				// 8MHz / 32 = 250kHz. 13 ADC clocks are required for a conversion,
+				// s 250kHz / 13 = ~19.23 kHz. (Periode of ~52uS)
+				ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);
+				state = STATE_HPD_RUNNING;
+				break;
+
+			case STATE_HPD_RUNNING:
+				ADCSRA |= (1<<ADSC);
+				while (ADCSRA & (1<<ADSC)) { }
+
+				value = ADCH;
+				value ^= 0xff;
+
+				PORTC = (value << 2) & 0x3c;
+				_delay_us(62);
+				//_delay_us(124);
+
+				PORTC = ((value >> 2) & 0x3c) | 0x02;
+				//_delay_us(72); // 124us - 52us (to compensate time spent in ADC)
+				_delay_us(10); // 124us - 52us (to compensate time spent in ADC)
+				break;
+
+			case STATE_EXPORT_INIT:
+				// Switch ADC to free running mode so we can concentrate on reacting
+				// quickly to transition on PORTD2
+				ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);
+				ADCSRA |= (1<<ADFR) | (1<<ADSC);
+				state = STATE_EXPORT_RUNNING;
+				break;
+
+			case STATE_EXPORT_RUNNING:
+
+				PORTC = ((value >> 2) & 0x3c) | 0x02;
+				while (PIND & 0x04) { }
+
+				// New value on falling edge
+				value = ADCH;
+				value ^= 0xff;
+
+				PORTC = (value << 2) & 0x3c;
+				while (!(PIND & 0x04)) { }
+				break;
+		}
+	}
+
+#if 0
+	/* The export paddle is supposed to follow transitions on pin 7.
+	 * But for test purposes when this pin is not wired, the hack
+	 * below only sends the upper nibble. So the only values sent
+	 * are 0x11, 0x22, ... 0xee, 0xff.
+	 *
+	 * This works, but the controller is not very usable
+	 * (only 16 directions instead of 256).
+	 *
+	 */
+	while(!export_paddle_mode)
+	{
+		ADCSRA |= (1<<ADSC);
+		while (ADCSRA & (1<<ADSC)) { }
+
+		value = ADCH;
+
+		PORTC = ((value >> 2) & 0x3c);
+		_delay_us(124);
+
+		PORTC = ((value >> 2) & 0x3c) | 0x02;
+		_delay_us(72); // 124us - 52us (to compensate time spent in ADC)
+	}
+#endif
+}
